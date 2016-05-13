e007142f0cbcb1410287ac1bbf937aaa0498926b
B10213147 <B10213147@yuntech.org.tw>
2016-05-13 13:35:55 +0200

    Finished sch_on()

--- rtos.c
+++ rtos.c
@@ -2,7 +2,7 @@
  * rtos.c
  *
  *  Created on: May 13, 2016
- *      Author: USER
+ *      Author: Harvard Tseng
  */
 
 #include "rtos.h"
@@ -11,6 +11,67 @@
 #include "hw_memmap.h"
 #include "sysctl.h"
 #include "timer.h"
+
+// System variables
+int sch_tst, sch_idx, slice_quantum;
+
+// Enable IRQ interrupts
+void enable_irq()
+{
+	asm(" stmfd sp!, {r0}");
+	asm(" mrs   r0, cpsr");
+	asm(" bic   r0, r0, #0x80");
+	asm(" msr   cpsr_c, r0");
+	asm(" ldmfd sp!, {r0}");
+}
+
+// Disable IRQ interrupts
+void disable_irq()
+{
+	asm(" stmfd sp!, {r0}");
+	asm(" mrs   r0, cpsr");
+	asm(" orr   r0, r0, #0x80");
+	asm(" msr   cpsr_c, r0");
+	asm(" ldmfd sp!, {r0}");
+}
+
+// Start real time operating system
+// slice ... timeslice in microseconds
+void sch_on(unsigned int slice){
+	sch_tst = task_completed;
+	sch_idx = 0;
+	slice_quantum = slice;
+
+	//
+	// Enable the peripherals used by this example.
+	//
+	SysCtlPeripheralEnable(SYSCTL_PERIPH_TIMER0);
+
+	//
+	// Check if the peripheral access is enabled.
+	//
+	while(!SysCtlPeripheralReady(SYSCTL_PERIPH_TIMER0))
+	{
+	}
+
+	//
+	// Configure the two 32-bit periodic timers.
+	//
+	TimerClockSourceSet(TIMER0_BASE, TIMER_CLOCK_SYSTEM);
+	TimerConfigure(TIMER0_BASE, TIMER_CFG_PERIODIC);
+	TimerIntRegister(TIMER0_BASE, TIMER_BOTH, sch_int);
+	TimerMatchSet(TIMER0_BASE, TIMER_BOTH, slice_quantum);
+
+	//
+	// Enable the timers.
+	//
+	TimerEnable(TIMER0_BASE, TIMER_BOTH);
+}
+
+// Real time operating system core
+void sch_int(){
+
+}
 
 
 
--- rtos.h
+++ rtos.h
@@ -2,12 +2,18 @@
  * rtos.h
  *
  *  Created on: May 13, 2016
- *      Author: USER
+ *      Author: Harvard Tseng
  */
 
 #ifndef RTOS_H_
 #define RTOS_H_
 
+// System status
+#define task_completed	0
+#define task_running	1
 
+// Function declarations
+extern void sch_on(unsigned int slice);
+extern void sch_int();
 
 #endif /* RTOS_H_ */
