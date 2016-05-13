0d3356a6cfb3a7924c6d9690af385dd7b8db3905
B10213147 <B10213147@yuntech.org.tw>
2016-05-13 13:57:40 +0200

    type define voidfuncptr

--- rtos_task.h
+++ rtos_task.h
@@ -2,12 +2,17 @@
  * rtos_task.h
  *
  *  Created on: May 13, 2016
- *      Author: USER
+ *      Author: Harvard Tseng
  */
 
 #ifndef RTOS_TASK_H_
 #define RTOS_TASK_H_
 
+typedef void (* voidfuncptr)();
+
+extern voidfuncptr priv_task;
+extern voidfuncptr sch_tab[];
+extern int sch_tab_size;
 
 
 #endif /* RTOS_TASK_H_ */
