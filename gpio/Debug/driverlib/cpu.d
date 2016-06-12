96f543674852a3cae4a11d2e0fb37b9fc86c6887
B10213147 <B10213147@yuntech.org.tw>
2016-06-13 05:37:29 +0800

    Deleted gpio folder

--- gpio/Debug/driverlib/cpu.d
+++
@@ -1,4 +0,0 @@
-driverlib/cpu.o: C:/ti/TivaWare_C_Series-2.1.2.111/driverlib/cpu.c \
- C:/ti/TivaWare_C_Series-2.1.2.111/driverlib/cpu.h
-
-C:/ti/TivaWare_C_Series-2.1.2.111/driverlib/cpu.h:
--- gpio/Debug/driverlib/sw_crc.d
+++
@@ -1,4 +0,0 @@
-driverlib/sw_crc.o: C:/ti/TivaWare_C_Series-2.1.2.111/driverlib/sw_crc.c \
- C:/ti/TivaWare_C_Series-2.1.2.111/driverlib/sw_crc.h
-
-C:/ti/TivaWare_C_Series-2.1.2.111/driverlib/sw_crc.h:
