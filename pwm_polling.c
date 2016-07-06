488c4441818739e14e1ad740abb16911c34c714c
B10213147 <B10213147@yuntech.org.tw>
2016-07-06 11:53:20 +0800

    Added two files

--- pwm_polling.c
+++ pwm_polling.c
@@ -1,107 +1,89 @@
 /*
- * main.c
+ * pwm_polling.c
+ *
+ *  Created on: July 6, 2016
+ *      Author: Harvard Tseng
  */
-#include <stdint.h>
-#include <stdbool.h>
-#include "hw_memmap.h"
-#include "gpio.h"
-#include "sysctl.h"
-#include "pwm.h"
 
-void startup(void);
-void pulse_train(void);
+#include "pwm_polling.h"
+#include "rtos.h"
+#include "TM4C123GH6PM.h"
+#include "driverlib/sysctl.h"
+#include "driverlib/pwm.h"
+#include "driverlib/gpio.h"
+#include "driverlib/pin_map.h"
 
-int frequency = 1;
-float duty = 0.1;
+void pwm_R(void){
+	// Enable the PWM generator
+	PWMGenEnable(PWM1_BASE, PWM_GEN_2);
+	// Turn on the Output pins
+	PWMOutputState(PWM1_BASE, PWM_OUT_5_BIT, true);
 
-int key_last_state = 0;
-int main(void) {
-	startup();
 
-	int key_state, period, width;
 
-	while(1){
-		key_state = ~(GPIOPinRead(GPIO_PORTB_BASE, GPIO_PIN_0) | GPIOPinRead(GPIO_PORTB_BASE, GPIO_PIN_1) |
-				GPIOPinRead(GPIO_PORTB_BASE, GPIO_PIN_2) | GPIOPinRead(GPIO_PORTB_BASE, GPIO_PIN_3));
-
-		//frequency increase 1Hz
-		if((key_state & GPIO_PIN_0 != 0) && (key_last_state & GPIO_PIN_0 == 0)){
-			frequency += 1;
-			if(frequency>10)
-			{frequency = 10;}
-		}
-		//frequency decrease 1Hz
-		if((key_state & GPIO_PIN_1 != 0) && (key_last_state & GPIO_PIN_1 == 0)){
-			frequency -= 1;
-			if(frequency<1)
-			{frequency = 1;}
-		}
-		//duty cycle increase 0.1
-		if((key_state & GPIO_PIN_2 != 0) && (key_last_state & GPIO_PIN_2 == 0)){
-			duty += 0.07;
-			if(duty>1)
-			{duty = 0.99;}
-		}
-		//duty cycle decrease 0.1
-		if((key_state & GPIO_PIN_3 != 0) && (key_last_state & GPIO_PIN_3 == 0)){
-			duty -= 0.07;
-			if(duty<0)
-			{duty = 0.01;}
-		}
-
-		key_last_state = key_state;
-		period = SysCtlPWMClockGet() / frequency;
-		width = period * duty;
-		PWMGenPeriodSet(PWM0_BASE, PWM_GEN_0, period);
-		PWMPulseWidthSet(PWM0_BASE, PWM_OUT_0, width);
-	}
+	// Turn off the Output pins
+	PWMOutputState(PWM1_BASE, PWM_OUT_5_BIT, false);
+	// Disable the PWM generator
+	PWMGenDisable(PWM1_BASE, PWM_GEN_2);
 }
 
-void startup(void){
-	//
-	// Set the clocking to run directly from the crystal.
-	//
-	SysCtlClockSet(SYSCTL_SYSDIV_1 | SYSCTL_USE_OSC | SYSCTL_OSC_MAIN |
-			SYSCTL_XTAL_16MHZ);
+void pwm_B(void){
+	// Enable the PWM generator
+	PWMGenEnable(PWM1_BASE, PWM_GEN_3);
+	// Turn on the Output pins
+	PWMOutputState(PWM1_BASE, PWM_OUT_6_BIT, true);
 
-	//
-	// Enable the GPIO port that is used for the on-board LED.
-	//
-	SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOB);
 
-	//
-	// Check if the peripheral access is enabled.
-	//
-	while(!SysCtlPeripheralReady(SYSCTL_PERIPH_GPIOF))
-	{
-	}
 
-	//
-	// Enable the GPIO pin for the LED (PF3).  Set the direction as output, and
-	// enable the GPIO pin for digital function.
-	//
-	GPIOPinTypeGPIOInput(GPIO_PORTB_BASE, GPIO_PIN_0 | GPIO_PIN_1 | GPIO_PIN_2 | GPIO_PIN_3);
-
-	//
-	// Enable the peripherals used by this example.
-	//
-	SysCtlPeripheralEnable(SYSCTL_PERIPH_PWM0);
-
-	//
-	// Check if the peripheral access is enabled.
-	//
-	while(!SysCtlPeripheralReady(SYSCTL_PERIPH_PWM0))
-	{
-	}
-
-	SysCtlPWMClockSet(SYSCTL_PWMDIV_4);	// 16MHz/4 = 4MHz
-
-	GPIOPinTypePWM(GPIO_PORTB_BASE, GPIO_PIN_6);
-
-	PWMGenConfigure(PWM0_BASE, PWM_GEN_0, PWM_GEN_MODE_UP_DOWN | PWM_GEN_MODE_NO_SYNC | PWM_GEN_MODE_DBG_STOP);
-
-	PWMGenEnable(PWM0_BASE, PWM_GEN_0);
-	PWMOutputState(PWM0_BASE, PWM_OUT_0_BIT, true);
+	// Turn off the Output pins
+	PWMOutputState(PWM1_BASE, PWM_OUT_6_BIT, false);
+	// Disable the PWM generator
+	PWMGenDisable(PWM1_BASE, PWM_GEN_3);
 }
+
+void pwm_G(void){
+	// Enable the PWM generator
+	PWMGenEnable(PWM1_BASE, PWM_GEN_3);
+	// Turn on the Output pins
+	PWMOutputState(PWM1_BASE, PWM_OUT_7_BIT, true);
+
+
+
+	// Turn off the Output pins
+	PWMOutputState(PWM1_BASE, PWM_OUT_7_BIT, false);
+	// Disable the PWM generator
+	PWMGenDisable(PWM1_BASE, PWM_GEN_3);
+}
+
+void pwm1_init(void){
+	//Configure PWM Clock to match system
+	SysCtlPWMClockSet(SYSCTL_PWMDIV_1);
+
+	// Enable the peripherals used by this program.
+	SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOF);
+	SysCtlPeripheralEnable(SYSCTL_PERIPH_PWM1);  //The Tiva Launchpad has two modules (0 and 1). Module 1 covers the LED pins
+
+	//Configure PF1,PF2,PF3 Pins as PWM
+	GPIOPinConfigure(GPIO_PF1_M1PWM5);
+	GPIOPinConfigure(GPIO_PF2_M1PWM6);
+	GPIOPinConfigure(GPIO_PF3_M1PWM7);
+	GPIOPinTypePWM(GPIOF_BASE, GPIO_PIN_1 | GPIO_PIN_2 | GPIO_PIN_3);
+
+	//Configure PWM Options
+	//PWM_GEN_2 Covers M1PWM4 and M1PWM5
+	//PWM_GEN_3 Covers M1PWM6 and M1PWM7 See page 207 4/11/13 DriverLib doc
+	PWMGenConfigure(PWM1_BASE, PWM_GEN_2, PWM_GEN_MODE_DOWN | PWM_GEN_MODE_NO_SYNC);
+	PWMGenConfigure(PWM1_BASE, PWM_GEN_3, PWM_GEN_MODE_DOWN | PWM_GEN_MODE_NO_SYNC);
+
+	//Set the Period (expressed in clock ticks)
+	PWMGenPeriodSet(PWM1_BASE, PWM_GEN_2, 320);
+	PWMGenPeriodSet(PWM1_BASE, PWM_GEN_3, 320);
+
+	//Set PWM duty-50% (Period /2)
+	PWMPulseWidthSet(PWM1_BASE, PWM_OUT_5,100);
+	PWMPulseWidthSet(PWM1_BASE, PWM_OUT_6,100);
+	PWMPulseWidthSet(PWM1_BASE, PWM_OUT_7,100);
+}
+
 
 
---
+++ pwm_polling.h
@@ -0,0 +1,17 @@
+/*
+ * pwm_polling.h
+ *
+ *  Created on: July 6, 2016
+ *      Author: Harvard Tseng
+ */
+
+#ifndef PWM_POLLING_H_
+#define PWM_POLLING_H_
+
+extern void pwm1_init(void);
+extern void pwm_R(void);
+extern void pwm_G(void);
+extern void pwm_B(void);
+
+
+#endif /* PWM_POLLING_H_ */
