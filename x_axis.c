eef89b1585f93739c8d700a86265367cca3df540
B10213147 <B10213147@yuntech.org.tw>
2016-07-31 03:22:46 +0800

    Added x_axis

---
+++ x_axis.c
@@ -0,0 +1,41 @@
+/*
+ * x_axis.c
+ *
+ *  Created on: July 31, 2016
+ *      Author: Harvard Tseng
+ */
+
+#include "x_axis.h"
+#include "TM4C123GH6PM.h"
+#include "driverlib/sysctl.h"
+#include "driverlib/timer.h"
+#include "driverlib/gpio.h"
+#include "driverlib/pin_map.h"
+#include "driverlib/interrupt.h"
+#include "driverlib/pwm.h"
+#include "inc/hw_ints.h"
+
+void x_pulse_Gen(void);
+void x_pwm_Start(void);
+void x_timer_End(void);
+
+void x_axis_Move(int pulses){
+
+}
+
+void x_axis_Init(void){
+
+}
+
+void x_pulse_Gen(void){
+
+}
+
+void x_pwm_Start(void){
+
+}
+
+void x_timer_End(void){
+
+}
+
---
+++ x_axis.h
@@ -0,0 +1,19 @@
+/*
+ * x_axis.h
+ *
+ *  Created on: July 31, 2016
+ *      Author: Harvard Tseng
+ */
+
+#ifndef X_AXIS_H_
+#define X_AXIS_H_
+
+#include <stdint.h>
+#include <stdbool.h>
+
+extern void x_axis_Init(void);
+extern void x_axis_Move(int pulses);
+
+
+
+#endif /* X_AXIS_H_ */
