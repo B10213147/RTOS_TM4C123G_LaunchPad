1bd8a56d4ff3da2bc282dcb780208e8219731ef3
B10213147 <B10213147@yuntech.org.tw>
2016-07-31 03:27:23 +0800

    Added y_axis

---
+++ y_axis.c
@@ -0,0 +1,41 @@
+/*
+ * y_axis.c
+ *
+ *  Created on: July 31, 2016
+ *      Author: Harvard Tseng
+ */
+
+#include "y_axis.h"
+#include "TM4C123GH6PM.h"
+#include "driverlib/sysctl.h"
+#include "driverlib/timer.h"
+#include "driverlib/gpio.h"
+#include "driverlib/pin_map.h"
+#include "driverlib/interrupt.h"
+#include "driverlib/pwm.h"
+#include "inc/hw_ints.h"
+
+void y_pulse_Gen(void);
+void y_pwm_Start(void);
+void y_timer_End(void);
+
+void y_axis_Move(int pulses){
+
+}
+
+void y_axis_Init(void){
+
+}
+
+void y_pulse_Gen(void){
+
+}
+
+void y_pwm_Start(void){
+
+}
+
+void y_timer_End(void){
+
+}
+
